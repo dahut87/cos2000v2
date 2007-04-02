7045741c5eac22b00e03afafd1bcb2a2fde3c1bf
Nicolas Hordé <nicolas@palon.fr>
2007-04-02 13:17:40 +0000

    feat: changement radical de syntaxe des fonctions

--- lib/memory.c
+++ lib/memory.c
@@ -1,38 +1,46 @@
 #include "types.h"
 
-void memset(void *dest, u8 val, u16 count, u32 size)
-{
- char *temp;
- for(temp = (u8 *)dest; count != 0; count--) 
-{
-*temp = val;
-temp+=size;
-}
-}
+/*******************************************************************************/
 
-void memcpy(void *dest, const void *src, u32 size)
+/* Copie un octet une ou plusieurs fois en mémoire */
+void memset(void *dst, u8 val, u32 count,u32 size)
 {
-	u8 *s, *d;
-	u32 i;
-	s = (u8 *) src;
-	d = (u8 *) dest;
-	for(i=0;i<size;i++){
-		*(d+i) = *(s+i);	
+	u8 *temp;
+	for(temp = (u8 *)dst; count != 0; count--)
+	{
+		temp+=size;
+		*temp = val;
 	}
 }
 
+/*******************************************************************************/
 
-
-int memcmp(const void *src, const void *dest, int count)
+/* Copie une portion de mémoire vers une autre */
+void memcpy(void *src, void *dst, u32 count, u32 size)
 {
- const unsigned char *mem1 = (const unsigned char *)src;
- const unsigned char *mem2 = (const unsigned char *)dest;
- for(; count != 0; count--)
- {
-  if(*mem1 != *mem2)
-  return *mem1 -  *mem2;
-  mem1++;
-  mem2++;
- }
+	char *s, *d;
+	u32 i;
+	s = (u8*) src;
+	d = (u8*) dst;
+	for(i=0;i<count;i++){
+		*(d+i*size) = *(s+i);	
+	}
 }
 
+/*******************************************************************************/
+
+/* Compare 2 portions de mémoire */
+u32 memcmp(void *src, void *dst, u32 count, u32 size)
+{
+	const u8 *mem1 = (const u8 *)src;
+	const u8 *mem2 = (const u8 *)dst;
+	for(; count != 0; count--)
+	{
+		if(*mem1 != *mem2)
+		return *mem1 -  *mem2;
+		mem1+=size;
+		mem2+=size;
+	}
+}
+
+/*******************************************************************************/
