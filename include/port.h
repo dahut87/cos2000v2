1917eef0c2a32ed759f024e5bfdde43f7c36ddff
Nicolas Hordé <nicolas@palon.fr>
2007-04-02 12:58:40 +0000

    feat: déclaration de fonctions ayant rapport avec la gestion des I/O

---
+++ include/port.h
@@ -0,0 +1,8 @@
+#include "types.h"
+
+void outreg(u16 port,u8 *src,u16 num);
+void outregsame(u16 port,u8 *src,u16 num);
+void inreg(u16 port,u8 *src,u16 num);
+void inregsame(u16 port,u8 *src,u16 num);
+void initreg(u16 port,u8 *src,u16 num);
+void initregsame(u16 port,u8 *src,u16 num);
