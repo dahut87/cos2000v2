1bfa59381eefb0b19b6ae492554ba30c9438e8ea
Nicolas Hordé <nicolas@palon.fr>
2007-04-02 13:14:21 +0000

    feat: ajout de nouvelles déclarations de fonctions vidéos

--- include/vga.h
+++ include/vga.h
@@ -2,15 +2,23 @@
 
 #define TEXTSCREEN 0xB8000	/* debut de la memoire video texte*/
 #define GRPHSCREEN 0xA0000	/* debut de la memoire video graphique*/
+typedef u8 mode_def[64];
 
-typedef u8 mode_def[64]; 
 
-void 	print (char* string);
-void 	clearscreen (void);
-u16 	setvmode(u8);
-void 	showchar (u8 thechar);
-void 	showhex (u8 src);
-void 	fillscreen (u8 color);
-u8 	getfont(u8 num);
-void 	setfont(u8 num);
-void 	gotoscreen(u16 x,u16 y);
+u32 setvmode(u8);
+u32 loadfont(u8* def,u8 size,u8 font);
+void gotoscr(u16 x,u16 y);
+void useplane(u8 plan);
+u8 getfont(u8 num);
+void setfont(u8 num);
+void waitvretrace (void);
+void waithretrace (void);
+void enablecursor (void);
+void disablecursor (void);
+void enablescroll (void);
+void disablescroll (void);
+void (*writepxl)(u16 x, u16 y, u32 c);
+void (*showchar)(u8 thechar);
+void (*cls)(void);
+void (*scroll)(u8 lines);
+
