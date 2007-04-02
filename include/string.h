659a895fdaf69ff9df9bcde2f2cc1a90a42f6e51
Nicolas Hordé <nicolas@palon.fr>
2007-04-02 14:00:50 +0000

    feat: déclaration de fonctions ayant rapport avec la gestion des chaînes de caractères

---
+++ include/string.h
@@ -0,0 +1,8 @@
+#include "types.h"
+
+s8 strcmp(const u8 *src,const u8 *des);
+u32 strlen(const u8 *src);
+u8 *strchr(const u8 *src, u8 achar);
+u8 *strncpy(const u8 *src,u8 *des,u32 count);
+u8 *strcat(const u8 *src,u8 *des);
+u8* strcpy(const u8 *src, u8 *des);
