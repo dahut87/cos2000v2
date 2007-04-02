b0441884e27256e230bee438d2d486c7d1628a4b
Nicolas Hordé <nicolas@palon.fr>
2007-04-02 13:26:03 +0000

    feat: test des nouvelles fonctionnalités graphiques de la librairie VGA

--- system/system.c
+++ system/system.c
@@ -1,9 +1,18 @@
 #include "vga.h"
+#include "video.h"
 
-int main(void) {
-	setvmode(0x1);
-	clearscreen();
-	setfont(1);
-	print("Hello cos is here");
-	while(1);
+int _main(void) {
+	setvmode(0x84);
+        cls();
+        int x,y;
+        for(x=0;x<120;x++)
+        for(y=0;y<120;y++)
+        {
+       	writepxl(x,y,4);
+        }
+        for(x=0;x<20000;x++)
+        {
+        print("C'est 2 test 0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789 c un test!");
+	}
+        while(1);	
 }
