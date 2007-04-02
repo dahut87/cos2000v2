fa3f9f8cab8088bd036bd75ed76614e31c680a28
Nicolas Hordé <nicolas@palon.fr>
2007-04-02 14:01:17 +0000

    feat: définition de nouveaux types

--- include/types.h
+++ include/types.h
@@ -4,7 +4,9 @@
 typedef unsigned char	u8;
 typedef unsigned short	u16;
 typedef unsigned int	u32;
-typedef unsigned char uchar;
+typedef char	s8;
+typedef short	s16;
+typedef int	s32;
 typedef int bool; 
 
 struct dtr {
