39822e8f9ea9b76f8c1705dee729dceeb0adab7f
Nicolas Hordé <nicolas@palon.fr>
2007-04-05 17:33:32 +0000

    feat: Préparation de la gestion de la mémoire (LDT/GDT)

---
+++ include/type.h
@@ -0,0 +1,31 @@
+/***********************************/
+/* Librairie Type.h                */
+/* 20 Mars 2007                    */
+/***********************************/
+
+#ifndef JREKCED_TYPE
+# define JREKCED_TYPE
+
+# define NULL (0x0LL)
+
+typedef char            	int8;
+typedef short           	int16;
+typedef int             	int32;
+typedef long long       	int64;
+
+typedef char	      	        BYTE;
+typedef short	      	        WORD;
+typedef long	      	        DWORD;
+typedef long long       	QWORD;
+
+typedef signed char		SBYTE;
+typedef signed short		SWORD;
+typedef signed long		SDWORD;
+typedef signed long long	SQWORD;
+
+typedef unsigned char		UBYTE;
+typedef unsigned short		UWORD;
+typedef unsigned long		UDWORD;
+typedef unsigned long long	UQWORD;
+
+#endif
